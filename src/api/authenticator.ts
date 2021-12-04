681d1f60b37fe0faee1baf80fadedfb5405f2437
Martin <martin.k738@gmail.com>
2021-12-04 12:12:24 +0100

    fixed typo

--- src/api/authenticator.ts
+++ src/api/authenticator.ts
@@ -3,11 +3,12 @@ import { IUserService } from '../services/interfaces/IUserService';
 
 export async function authenticateUser(request: Request, response: Response, userService: IUserService): Promise<number | null> {
     let authenticatedUserId = await userService.authenticate(request.headers.authorization);
-    if (!authenticatedUserId) this.unathorized(response);
+    if (!authenticatedUserId)
+        unauthorized(response);
     return authenticatedUserId;
 }
 
-function unathorized(response: Response): void {
+function unauthorized(response: Response): void {
     response.status(401);
     response.end();
 }
