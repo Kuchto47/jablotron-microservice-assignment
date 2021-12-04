91f832ca03852fceaad79688287935673a4afa62
Martin <martin.k738@gmail.com>
2021-12-04 13:56:00 +0100

    edited jsdocs in IEndpointProbe

--- src/backend-service/IEndpointProbe.ts
+++ src/backend-service/IEndpointProbe.ts
@@ -6,18 +6,21 @@ export interface IEndpointProbe {
 
     /**
      * Load Endpoint with given ID from DB and start monitoring
+     * 
      * @param endpointId ID of newly inserted endpoint to monitor
      */
     onInsert(endpointId: number): void;
 
     /**
      * Load Endpoint with given ID from DB and update data for monitoring
+     * 
      * @param endpointId ID of updated endpoint
      */
     onUpdate(endpointId: number): void;
 
     /**
      * Stop monitoring Endpoint with given ID and remove it from monitoring
+     * 
      * @param endpointId ID of recently deleted endpoint
      */
     onDelete(endpointId: number): void;
