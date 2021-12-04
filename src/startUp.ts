14d53f6f12b960bc2f62e7b3ffdbda4e135e128d
Martin <martin.k738@gmail.com>
2021-12-04 23:38:03 +0100

    removed obsolete todo comment

--- src/startUp.ts
+++ src/startUp.ts
@@ -72,7 +72,6 @@ export class StartUp {
     }
 
     private static registerControllers(server: Server): void {
-        /*TODO: Controllers should get on-methods of EndpointProbe as dependencies too */
         let controllers: IBaseController[] = [
             new EndpointController(
                 server,
